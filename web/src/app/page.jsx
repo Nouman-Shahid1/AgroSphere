df61bb68d83b603c0181c81c9b325cfee57722db
Nouman-Shahid1 <muhammadnoumansha140@gmail.com>
2025-01-29 00:33:27 +0500

    tailwind error resolved

--- web/src/app/page.jsx
+++ web/src/app/page.jsx
@@ -1,12 +1,12 @@
 'use client';
 
-import Navbar from "../src/Components/Navbar/Navbar";
+import Navbar from "../Components/Navbar/Navbar";
 export default function Page() {
   return (
     <div>
       <Navbar/>
       <main>
-        <h1 className="text-center mt-10 text-2xl taxt-green-600">Welcome to AgroSphere</h1>
+        <h1 className="text-center mt-10 text-2xl text-green-600">Welcome to AgroSphere</h1>
         <p className="text-center text-lg mt-4">
           Explore the best agricultural solutions for your needs.
         </p>
